110e004e4c5502b777cf27c1b73c135c57809542
Malatesh bn <malateshbn178@gmail.com>
2026-07-11 10:53:08 +0530

    Update PatternObject.py

--- RegularExpression/PatternObject.py
+++ RegularExpression/PatternObject.py
@@ -1,27 +1,25 @@
 import re
 
-# text = "7676115923 76761123"
-# regex = r"\d{10}"
-# print(re.search(regex,text))
+text = "7676115923 76761123"
+regex = r"\d{10}"
+print(re.search(regex,text))
 
 # or
 
-# text = "7676115923 76761123 9686520989"
-# p= re.compile(r"\d{10}")
-# print(p.findall(text))
+text = "7676115923 76761123 9686520989"
+p= re.compile(r"\d{10}")
+print(p.findall(text))
 
-# text = ['7676115923',
-# '9686520989',
-# '9448344099',
-# '8197344099',
-# '914808162',
-# '89288188639']
-# p = re.compile(r"\b\d{5}[02468]\d{4}\b")
+text = ['7676115923',
+'9686520989',
+'9448344099',
+'8197344099',
+'914808162',
+'89288188639']
+p = re.compile(r"\b\d{5}[02468]\d{4}\b")
 
-# for i in text:
-#     if p.search(i) != None:
-#         print(i,"valid")
-#     else:
-#         print(i,"not valid")
-
-# program to print name and age
+for i in text:
+    if p.search(i) != None:
+        print(i,"valid")
+    else:
+        print(i,"not valid")
