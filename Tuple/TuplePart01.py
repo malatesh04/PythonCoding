b8bb6e859b46efe904c0826fc6a85d6225a9de16
Malatesh bn <malateshbn178@gmail.com>
2026-07-15 10:41:13 +0530

    Add TuplePart01.py

---
+++ Tuple/TuplePart01.py
@@ -0,0 +1,15 @@
+# 'in' operator --> CHECK MEMBERSHIP
+lst = [10,20,30,40,50,60]
+print(40 in lst) 
+# TIME COMPLEXITY --> LINEAR TIME --> O{N}
+
+# CONTATINATION --> CONCATE TWO LIST
+LST1 = [10,20,30]
+LST2 = [40,50,60]
+print(LST1+LST2)
+# TIME COMPLEXITY --> LINERAR TIME --> O(N1+N2)
+
+# REPLICATION OF LIST --> *
+LST1 = [10,20] * 10
+print(LST1)
+# TIME COMPLEXITY --> LINEAR TIME --> O{c*n} --> o{constant * number}
