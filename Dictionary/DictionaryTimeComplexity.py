1086492b8dd23a671815ce2ddd9082ff8e2d0730
Malatesh bn <malateshbn178@gmail.com>
2026-07-16 15:21:27 +0530

    Update Dictionary.py and add DictionaryTi

---
+++ Dictionary/DictionaryTimeComplexity.py
@@ -0,0 +1,19 @@
+d = {2:45,1:92,16:64,22:56,18:72,26:88}
+print(d[1])
+print(26 in d)
+print(len(d))
+
+d = {1:'c',2:'java',3:'python'}
+print(d)
+d[4] ='c++'
+print(d)
+d.update({5:'html',6:'css'})
+print(d)
+d.update(seven='js',eight='php')
+print(d)
+d.update({1:'html'})
+print(d)
+# d.pop(1)
+# print(d)
+ 
+
