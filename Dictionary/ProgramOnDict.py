bfdc680372987fce28a74909043024c110640bd3
Malatesh bn <malateshbn178@gmail.com>
2026-07-18 10:40:00 +0530

    Update ProgramOnDict.py

--- Dictionary/ProgramOnDict.py
+++ Dictionary/ProgramOnDict.py
@@ -5,7 +5,6 @@ s = re.sub(r'[.,?!]',' ',s)
 lst = s.split()
 d = {}
 count = 0
-
 for i in lst:
     if i not in d:
         d[i] = 1
@@ -15,3 +14,15 @@ for i in lst:
         count +=1
 print(d)
 print(count) 
+
+
+d = {1:'A',2:'B',3:'A',4:'B',5:'B',6:'C'}
+res = {}
+
+for i in d:
+    if d[i] not in res:
+        res[d[i]] = []
+        res[d[i]].append(i)
+    else:
+        res[d[i]].append(i)
+print(res)
