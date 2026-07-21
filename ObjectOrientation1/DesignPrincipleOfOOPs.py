958ee8563fa2a373f0ea6342cc36e3714bdaa291
Malatesh bn <malateshbn178@gmail.com>
2026-07-21 19:41:49 +0530

    Add DesignPrincipleOfOOPs.py

---
+++ ObjectOrientation1/DesignPrincipleOfOOPs.py
@@ -0,0 +1,17 @@
+'''
+
+--> oops :  is a programming approch that uses classes and object to write programs helps make code reusable,secure, easy to maintain.
+
+four pillars :
+
+1) encapsulation --> wrapping data and methods into a single unit.
+2)Abstaction --> Hiding implementaion details and only necessary features.
+3)inheritance --> one class can inherit the properties and methods of another class
+4)polymorphism --> the same method can behave differently in different situvation
+
+advatages --> 
+1)Code Reusability
+2)Easy Maintenance
+3)Easy to Add New Features
+'''
+
